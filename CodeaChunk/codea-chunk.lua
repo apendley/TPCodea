5eb53867900cbe8a7bc9e2938e81c1e7fd2ea85c
Aaron Pendley <aaronpendley@gmail.com>
2012-11-05 09:51:09 -0500

    - use ["key"] syntax instead of dot syntax for table name and sprite frame names, in case either of them include spaces

--- CodeaChunk/codea-chunk.lua
+++ CodeaChunk/codea-chunk.lua
@@ -11,7 +11,8 @@ return {
 	smartUpdateKey="{{smartUpdateKey}}",
 	texture={name="{{texture.trimmedName }}", size={w={{texture.size.width}},h={{texture.size.height}}}},
 	frames={{% for sprite in allSprites %}
-		{{sprite.trimmedName}}={
-			trimmed={{sprite.trimmed}}, rotated={{sprite.rotated}}, sourceSize={w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}}}, frameSize={w={{sprite.frameRect.width}},h={{sprite.frameRect.height}}},
-			uvRect={s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}}} },{% endfor %} }
+		["{{sprite.trimmedName}}"]={ trimmed={{sprite.trimmed}}, rotated={{sprite.rotated}},
+			sourceSize={w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}}}, frameSize={w={{sprite.frameRect.width}},h={{sprite.frameRect.height}}},
+			uvRect={s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}}} },{% endfor %}
+	}
 }
--- CodeaTab/codea-tab.lua
+++ CodeaTab/codea-tab.lua
@@ -2,11 +2,12 @@
 -- created with TexturePacker (http://www.texturepacker.com)
 tp = tp or {}
 
-tp.{{texture.trimmedName}} = {
+tp["{{texture.trimmedName}}"] = {
 	smartUpdateKey="{{smartUpdateKey}}",
 	texture={name="{{texture.trimmedName }}", size={w={{texture.size.width}},h={{texture.size.height}}}},
 	frames={{% for sprite in allSprites %}
-		{{sprite.trimmedName}}={
-			trimmed={{sprite.trimmed}}, rotated={{sprite.rotated}}, sourceSize={w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}}}, frameSize={w={{sprite.frameRect.width}},h={{sprite.frameRect.height}}},
-			uvRect={s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}}} },{% endfor %} }
+		["{{sprite.trimmedName}}"]={ trimmed={{sprite.trimmed}}, rotated={{sprite.rotated}},
+			sourceSize={w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}}}, frameSize={w={{sprite.frameRect.width}},h={{sprite.frameRect.height}}},
+			uvRect={s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}}} },{% endfor %}
+	}
 }
