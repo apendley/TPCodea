7a93ba32fdb3da1951fa7f52ece040b124a602fd
Aaron Pendley <aaronpendley@gmail.com>
2012-11-06 10:27:15 -0500

    - inserted spaces at the beginning and end of each table definition for the project tab exporter. In some cases the Django template system gets confused when there are more than 2 unseparated end braces - made the chunk exporter more easily readable by humans (i.e. spread out over more lines), since this format is not intended to be imported as a Codea project tab

--- CodeaChunk/codea-chunk.lua
+++ CodeaChunk/codea-chunk.lua
@@ -8,12 +8,19 @@
 -- {{texture.trimmedName}} = assert(loadString(data))()
 
 return {
-	smartUpdateKey="{{smartUpdateKey}}",
-	version = 0,	
-	texture={name="{{texture.trimmedName}}", size={w={{texture.size.width}},h={{texture.size.height}}}},
+	smartUpdateKey = "{{smartUpdateKey}}",
+	version = 0,
+	texture = {
+		name = "{{texture.trimmedName}}",
+		size = { w = {{texture.size.width}}, h = {{texture.size.height}} } 
+	},
 	frames={{% for sprite in allSprites %}
-		["{{sprite.trimmedName}}"]={ trimmed={{sprite.trimmed}}, rotated={{sprite.rotated}},
-			sourceSize={w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}}}, frameSize={w={{sprite.frameRect.width}},h={{sprite.frameRect.height}}},
-			uvRect={s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}}} },{% endfor %}
+		["{{sprite.trimmedName}}"] = {
+			trimmed = {{sprite.trimmed}},
+			rotated = {{sprite.rotated}},
+			sourceSize = { w = {{sprite.untrimmedSize.width}}, h = {{sprite.untrimmedSize.height}} },
+			frameSize={ w = {{sprite.frameRect.width}}, h = {{sprite.frameRect.height}} },
+			uvRect = { s = {{sprite.frameRect.x}}/{{texture.size.width}}, t = 1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}), tw = {{sprite.frameRect.width}}/{{texture.size.width}}, th = {{sprite.frameRect.height}}/{{texture.size.height}} }
+		},{% endfor %}
 	}
 }
--- CodeaTab/codea-tab.lua
+++ CodeaTab/codea-tab.lua
@@ -4,11 +4,11 @@ tp = tp or {}
 
 tp["{{texture.trimmedName}}"] = {
 	smartUpdateKey="{{smartUpdateKey}}",
-	version = 0,
-	texture={name="{{texture.trimmedName}}", size={w={{texture.size.width}},h={{texture.size.height}}}},
+	version=0,
+	texture={ name="{{texture.trimmedName}}", size={ w={{texture.size.width}},h={{texture.size.height}} } },
 	frames={{% for sprite in allSprites %}
 		["{{sprite.trimmedName}}"]={ trimmed={{sprite.trimmed}}, rotated={{sprite.rotated}},
-			sourceSize={w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}}}, frameSize={w={{sprite.frameRect.width}},h={{sprite.frameRect.height}}},
-			uvRect={s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}}} },{% endfor %}
+			sourceSize={ w={{sprite.untrimmedSize.width}},h={{sprite.untrimmedSize.height}} }, frameSize={ w={{sprite.frameRect.width}},h={{sprite.frameRect.height}} },
+			uvRect={ s={{sprite.frameRect.x}}/{{texture.size.width}},t=1-(({{sprite.frameRect.y}}+{{sprite.frameRect.height}})/{{texture.size.height}}),tw={{sprite.frameRect.width}}/{{texture.size.width}},th={{sprite.frameRect.height}}/{{texture.size.height}} } },{% endfor %}
 	}
 }
